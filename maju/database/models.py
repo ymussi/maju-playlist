7a5be5fa7ee8e3fd0d65b759b3830342af1252b3
Yuri Mussi <mussi@MacBook-Air-de-Yuri.local>
2020-06-29 16:30:57 -0300

    change timezone

--- maju/database/models.py
+++ maju/database/models.py
@@ -1,10 +1,10 @@
 from sqlalchemy import Column, Integer, String, JSON, DateTime, Float
 from sqlalchemy.sql import func
 from maju.database import Base, Model
+import pytz
 
 
 class LogCityTemp(Model):
-    
     __tablename__ = 'log_city_temp'
     id = Column(Integer, nullable=False, unique=True, primary_key=True, autoincrement=True)
     city = Column(String(244))
@@ -13,6 +13,6 @@ class LogCityTemp(Model):
     music_gender = Column(String(244))
     provider = Column(String(244))
     status = Column(String(244))
-    created = Column(DateTime(timezone=True), default=datetime.now())
+    created = Column(DateTime(timezone=True), server_default=func.now(tz=pytz.timezone('America/Sao_Paulo')))
     weather_response = Column(JSON())
     provider_response = Column(JSON())
